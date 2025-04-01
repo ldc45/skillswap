8bd89158fe364034efa44dc0a3d79346aa330b86
Arnaud DELANNOY <arnauddel74@gmail.com>
2025-04-01 09:31:24 +0200

    Feat: AuthService now checks password with bcrypt

--- skillswap-api/src/auth/auth.service.ts
+++ skillswap-api/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './types/jwt-payload';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,8 +13,13 @@ export class AuthService {
 
   async signIn(email: string, pass: string) {
     const user = await this.userService.findOneByMail(email);
-    if (user?.password !== pass) {
+    if (!user) {
       throw new UnauthorizedException();
+    } else {
+      const isPassCorrect = await bcrypt.compare(pass, user.password);
+      if (!isPassCorrect) {
+        throw new UnauthorizedException();
+      }
     }
 
     const payload = { id: user.id, email: user.email } as JwtPayload;
