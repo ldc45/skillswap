94344efd4c0585efd2ff30aa708f76adf8c8a96e
Ludovic AGESILAS <98770184+ldc45@users.noreply.github.com>
2025-04-02 10:43:49 +0200

    Feat : add cors configuration

--- skillswap-api/src/main.ts
+++ skillswap-api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
