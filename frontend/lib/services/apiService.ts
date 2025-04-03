29696bcf038b9bd0330bce25fdf67c2a075e7f69
QuentinJoanon <quentin.joanon@gmail.com>
2025-04-03 15:42:33 +0200

    Env: add variable for frontend

--- frontend/lib/services/apiService.ts
+++ frontend/lib/services/apiService.ts
@@ -1,6 +1,6 @@
 import { useAuthStore } from "../stores/authStore";
 
-const API_URL = "http://localhost:4000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Variable pour suivre si une demande de rafraîchissement est en cours
 let isRefreshing = false;
