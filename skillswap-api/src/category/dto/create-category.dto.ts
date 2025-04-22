d05378cc4d38b981b016523b241422230000cdaf
Arnaud DELANNOY <arnauddel74@gmail.com>
2025-04-22 09:36:49 +0200

    Made CreateCategoryDto with Swagger & Class Validator

--- skillswap-api/src/category/dto/create-category.dto.ts
+++ skillswap-api/src/category/dto/create-category.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @ApiProperty({
+    example: 'Technology',
+    description: 'The name of the category',
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: '#ffd700',
+    description: 'The color of the category',
+  })
+  @IsOptional()
+  @IsString()
+  color: string | undefined;
+}
