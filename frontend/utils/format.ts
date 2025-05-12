6f6a9040069aeeff6936e19a8a4f181fb1d21ca9
Edrick101 <edrick.troupe.dev@gmail.com>
2025-05-12 11:11:32 +0200

    Fix: Prevent overlapping availability being added

--- frontend/utils/format.ts
+++ frontend/utils/format.ts
@@ -12,3 +12,15 @@ export function getFormattedDate(date: Date): string {
     minute: "2-digit",
   });
 }
+
+/**
+ * This function formats a given date to a number representing the hours and minutes.
+ * @param {Date} slotDate - The date to format
+ * @returns {Number} The formatted date number
+ * @example
+ * const slotDate = Mon May 12 2025 16:45:00 GMT+0200 (heure d’été d’Europe centrale)
+ * console.log(getFormattedTimeSlot(date)); // 16.75
+ */
+export function getFormattedTimeSlot(slotDate: Date): number {
+  return slotDate.getHours() + slotDate.getMinutes() / 60;
+}
