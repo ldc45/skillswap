f84d581adf6930ec68452254f759403d5a7af07a
Edrick101 <edrick.troupe.dev@gmail.com>
2025-05-13 13:44:04 +0200

    Fix: Responsive Design on user pages & code refactoring

--- frontend/components/partnerProfile/PartnerProfile.tsx
+++ frontend/components/partnerProfile/PartnerProfile.tsx
@@ -1,9 +1,8 @@
 import { User } from "@/@types/api";
+import UserSkills from "@/components/userSkills/UserSkills";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
 
 export default function PartnerProfile({ partner }: { partner: User }) {
-
   return (
     <div className="basis-1/2 p-4 flex flex-col gap-y-4 items-center">
       <div className="flex flex-col gap-y-2 items-center">
@@ -17,19 +16,7 @@ export default function PartnerProfile({ partner }: { partner: User }) {
         </h3>
       </div>
 
-      <div className="flex-wrap flex gap-y-1 gap-x-2">
-        {partner.skills && partner.skills.length > 0 && partner.skills.map((skill) => (
-          <Badge
-            variant="badge"
-            key={skill.id}
-            className="md:text-sm lg:text-base md:px-2 lg:px-4"
-          >
-            {skill.name && skill.name.length > 8
-              ? skill.diminutive || skill.name
-              : skill.name || skill.diminutive}
-          </Badge>
-        ))}
-      </div>
+      <UserSkills skills={partner.skills || []} />
 
       <div className="flex w-full flex-col gap-y-1">
         <h4 className="text-lg md:text-xl lg:text-2xl font-medium">
