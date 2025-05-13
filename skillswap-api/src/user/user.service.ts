107055620931c107434b0994222422b1df536a69
Arnaud DELANNOY <arnauddel74@gmail.com>
2025-05-13 10:35:26 +0200

    Feat: Added basic caching to UserService with cache reset when data changes

--- skillswap-api/src/user/user.service.ts
+++ skillswap-api/src/user/user.service.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -15,6 +16,8 @@ import { RequestCookies } from '../auth/types/request-cookies';
 import { UserResponseDto } from './dto/user-response.dto';
 import { plainToInstance } from 'class-transformer';
 import { UserWithRelations } from './type/user-with-relations';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class UserService {
@@ -22,6 +25,7 @@ export class UserService {
   constructor(
     private prisma: PrismaService,
     private jwtService: JwtService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
@@ -29,6 +33,9 @@ export class UserService {
     if (emailUser) {
       throw new BadRequestException('Email is already used.');
     }
+
+    // Delete users cache to force re-fetch
+    await this.cacheManager.del('users');
 
     createUserDto.password = await bcrypt.hash(
       createUserDto.password,
@@ -40,23 +47,31 @@ export class UserService {
   }
 
   async findAll(randomNum: number = 0): Promise<UserResponseDto[]> {
-    const users = await this.prisma.user.findMany({
-      include: {
-        skills: {
-          select: {
-            skill: true,
+    // Get users from cache
+    let users: UserWithRelations[] | null =
+      await this.cacheManager.get('users');
+
+    // If there is no user cache, fetch from Prisma and set cache
+    if (!users) {
+      users = await this.prisma.user.findMany({
+        include: {
+          skills: {
+            select: {
+              skill: true,
+            },
+          },
+          availabilities: {
+            select: {
+              id: true,
+              day: true,
+              startTime: true,
+              endTime: true,
+            },
           },
         },
-        availabilities: {
-          select: {
-            id: true,
-            day: true,
-            startTime: true,
-            endTime: true,
-          },
-        },
-      },
-    });
+      });
+      await this.cacheManager.set('users', users);
+    }
 
     if (randomNum > 0) {
       const randomUsers: UserWithRelations[] = [];
@@ -74,6 +89,22 @@ export class UserService {
   }
 
   async findOne(id: string): Promise<UserResponseDto | null> {
+    // Get cached users
+    const cacheUsers: UserWithRelations[] | null =
+      await this.cacheManager.get('users');
+
+    // If there is cache & demanded user is in it, filter and return it
+    if (cacheUsers) {
+      const cacheUser: UserWithRelations = cacheUsers.filter((usr) => {
+        return usr.id === id;
+      })[0];
+
+      if (cacheUser) {
+        return plainToInstance(UserResponseDto, cacheUser);
+      }
+    }
+
+    // Otherwise, fetch from Prisma and return
     const user = await this.prisma.user.findUnique({
       where: {
         id: id,
@@ -139,7 +170,10 @@ export class UserService {
     }
   }
 
-  update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
+    // Delete users cache to force re-fetch
+    await this.cacheManager.del('users');
+
     return this.prisma.user.update({
       where: {
         id: id,
@@ -148,7 +182,10 @@ export class UserService {
     });
   }
 
-  remove(id: string) {
+  async remove(id: string) {
+    // Delete users cache to force re-fetch
+    await this.cacheManager.del('users');
+
     return this.prisma.user.delete({
       where: {
         id: id,
